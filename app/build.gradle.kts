9abdedc229dc9f3611a2388f78e0ffe2ad1ded3c
Adrian Rocha <adrianrochacon@gmail.com>
2025-07-10 13:39:11 -0600

    Avance en pantalla principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,9 @@ android {
     buildFeatures {
         compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10" // o el más reciente
+    }
 }
 
 dependencies {
@@ -54,4 +57,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 }
